8e9490ec5ea35ba907ddd47e9638ac7963aadd54
Rione <anderson020398.007@gmail.com>
2025-10-09 00:41:21 -0500

    fragment de rehab listo con su respectivo listado

---
+++ app/src/main/java/com/dam/musicapp/model/Rehab.kt
@@ -0,0 +1,26 @@
+package com.dam.musicapp.model
+
+import com.dam.musicapp.R
+
+class Rehab {
+    var id: Int = 0
+    var name: String = ""
+    var imgRes: Int = 0
+
+    constructor(id: Int, name: String, imgRes: Int) {
+        this.id = id
+        this.name = name
+        this.imgRes = imgRes
+    }
+
+    companion object{
+        fun getRehabs(): List<Rehab>{
+            return listOf(
+                Rehab(1,"Rehabilitacion de rodilla", R.drawable.ic_rodilla),
+                Rehab(2,"Rehabilitacion de hombro",R.drawable.ic_hombro),
+                Rehab(3,"Rehabilitacion de espalda",R.drawable.ic_espalda),
+                Rehab(4,"Rehabilitacion de tobillo",R.drawable.ic_tobillo)
+            )
+        }
+    }
+}
--- app/src/main/java/com/dam/musicapp/ui/adapter/RehabAdapter.kt
+++ app/src/main/java/com/dam/musicapp/ui/adapter/RehabAdapter.kt
@@ -1,4 +1,32 @@
 package com.dam.musicapp.ui.adapter
 
-class RehabAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.dam.musicapp.R
+import com.dam.musicapp.model.Rehab
+
+class RehabAdapter(private val rehabs:List<Rehab>): RecyclerView.Adapter<RehabAdapter.RehabViewHolder>() {
+
+    class RehabViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val txtNameRehab: TextView = itemView.findViewById(R.id.txtNameRehab)
+        val icRehabImg: ImageView = itemView.findViewById(R.id.icRehabImg)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RehabViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_rehab_card, parent, false)
+        return RehabViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: RehabViewHolder, position: Int) {
+        val rehab = rehabs[position]
+        holder.txtNameRehab.text = rehab.name
+        holder.icRehabImg.setImageResource(rehab.imgRes)
+    }
+
+    override fun getItemCount(): Int = rehabs.size
 }
