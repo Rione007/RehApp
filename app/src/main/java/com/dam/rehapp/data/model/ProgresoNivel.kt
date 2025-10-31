7b8e28bd66c30ca784eb4b34569c4d247d5e0ec2
LeytonJulio <julio_cancer_999@hotmail.com>
2025-10-31 01:34:46 -0500

    Progress agregado

--- app/src/main/java/com/dam/rehapp/data/model/ProgresoNivel.kt
+++ app/src/main/java/com/dam/rehapp/data/model/ProgresoNivel.kt
@@ -3,5 +3,5 @@ package com.dam.rehapp.data.model
 data class ProgresoNivel(
     val nivelId: Int,
     var progreso: Int,
-    var tiempoTotal: Long
+    var tiempoTotalMin: Long //  Tiempo total visto en minutos
 )
